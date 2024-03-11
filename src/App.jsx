fdadd7db2f5773326a1d066a4fbd4db73de3715c
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2024-03-11 23:31:43 +0530

    Add a full screen canvas

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app">
+      <canvas className='canvas' />
+    </div>
+  );
+}
+
+export default App;
